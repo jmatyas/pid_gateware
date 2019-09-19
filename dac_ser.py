8cba2211b8d5829f26e090cbcf93658ab4e77808
JMatyas <jakubk.m@gmail.com>
2019-09-19 09:27:13 +0200

    dac_ser works. Data shouldnt lag one cycle

--- dac_ser.py
+++ dac_ser.py
@@ -2,6 +2,11 @@ from migen import *
 
 from collections import namedtuple
 
+DACParams = namedtuple("DACParams", [
+    "t_data",       # amount of clk cycles needed for data to be transmitted succesfully
+    "t_ldaclow",    # amount of clk cycles needed for DAC to react on ldac driven low (minimum)
+    "t_sync_ldac",  # amount of clk cylces between rising edge of sync and falling edge of ldac (minimum)
+])
 
 class DAC(Module):
     def __init__(self, pads, ch):
@@ -18,14 +23,15 @@ class DAC(Module):
         group = Signal(3)
         channel =  Signal(3)
 
-        ldac_duration = Signal(2)   # duration of ldac signal is needed to be longer than just a duration of state
-        bits_amount = Signal(max=2+6+16)   # counting bits left to transmit
-
         # new clock domain - for SPI purpose clk frequency of max 50 MHz is needed.
 
         clk_counter = Signal(max=2, reset_less=True)
         clk_tick = Signal()
         clk_toggle = Signal()
+        
+        count = Signal(max=24*4)       # hardcoded biggest value of the times
+        count_done = Signal()
+        count_load = Signal.like(count)
         
         ###
         
@@ -40,45 +46,52 @@ class DAC(Module):
             )            
         ]
 
+        self.comb += count_done.eq(count == 0)
+        self.sync += [
+                count.eq(count - 1),
+                If(count_done,
+                    count.eq(count_load),
+                )
+        ]
+
         # concatenating mode, address and data to be sent via SPI
         self.comb += group.eq(1), channel.eq(ch), mode.eq(3)
 
         self.submodules.fsm = fsm = FSM(reset_state="IDLE")
 
-        self.comb += pads.sclk.eq(clk_toggle)       # assigning clk_toggle to output clock to the device
+        self.comb += pads.sclk.eq(clk_toggle)                               # assigning clk_toggle to output clock to the device
 
         fsm.act ("IDLE",
             self.ready.eq(1),
                 If((self.start & self.dav), 
+                    count_load.eq(24*4 - 1),                                # load t_data to counter
                     NextState("DATA"),
                     NextValue(dataOut, Cat(self.data, address, mode)),      # latching data to transmit
-                    NextValue(bits_amount, 23),
                     self.ready.eq(0),
-                    pads.syncr.eq(0),       # chip select needs to be driven low
+                    pads.syncr.eq(0),                                       # chip select needs to be driven low
                 )
         )
         fsm.act("DATA",
             pads.syncr.eq(0),
-            If(clk_tick & clk_toggle,
-                If(bits_amount == 0,        # if all bits have been transmitted, change state
-                    NextState("BUSY"),
-                    NextValue(ldac_duration, 2)
-                ).Else(
-                    NextValue(bits_amount, bits_amount - 1),
-                    NextValue(pads.sdi, dataOut[0]),
-                    NextValue(dataOut, Cat(dataOut[1:], 0))
+            If(count_done,                                                  # if all bits have been transmitted, change state
+                NextState("BUSY"),
+                count_load.eq(3-1),                                         # load t_sync_ldac to counter
+            ).Else(
+                pads.sdi.eq(dataOut[0]),
+                If(clk_toggle & clk_tick,
+                    NextValue(dataOut, (Cat(dataOut[1:], 0)))
                 )
             )
         )
-        # here not pads busy is needed, but for the purpose of simulation is positive
+        # here ~pads.busy is needed, but for the purpose of simulation is positive
         fsm.act("BUSY",
-            If(pads.busy,
-                pads.ldac.eq(0),
-                If(ldac_duration == 0,
-                    NextState("IDLE")
-                ).Else(
-                    NextValue(ldac_duration, ldac_duration - 1)
+            If(count_done,
+                count_load.eq(2-1),                                         # load t_ldac to counter
+                If(pads.busy,
+                    pads.ldac.eq(0),
+                    If(count_done,
+                        NextState("IDLE")
+                    )
                 )
             )
         )
-
